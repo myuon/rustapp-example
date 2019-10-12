29a86f1aa7b77600132792e220024b63f8fba437
myuon <ioi.joi.koi.loi@gmail.com>
2019-10-12 18:23:11 +0900

    refactor: domain/service directory

---
+++ src/domain/service/user_service.rs
@@ -0,0 +1,39 @@
+use crate::domain::interface::IUserRepository;
+use crate::domain::model;
+use serde::*;
+use std::sync::Arc;
+
+#[derive(Clone)]
+pub struct UserService {
+    user_repository: Arc<dyn IUserRepository + Sync + Send>,
+}
+
+#[derive(Deserialize)]
+pub struct UserCreateInput {
+    pub name: String,
+    pub display_name: String,
+}
+
+impl UserService {
+    pub fn new(user_repository: Arc<dyn IUserRepository + Sync + Send>) -> UserService {
+        UserService {
+            user_repository: user_repository,
+        }
+    }
+
+    pub async fn create(&self, input: UserCreateInput) {
+        self.user_repository
+            .save(model::User {
+                id: ulid::Ulid::new().to_string(),
+                name: input.name,
+                display_name: input.display_name,
+                role: model::Role::Unknown,
+            })
+            .await
+            .unwrap()
+    }
+
+    pub async fn list(&self) -> Vec<model::User> {
+        self.user_repository.list().await.unwrap()
+    }
+}

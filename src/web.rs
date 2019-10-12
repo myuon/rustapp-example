edad478a11a53f94f6e9f32b0e4f9cfbf5559a7a
myuon <ioi.joi.koi.loi@gmail.com>
2019-10-12 16:14:34 +0900

    fix: handle bad request error

--- src/web.rs
+++ src/web.rs
@@ -1,7 +1,7 @@
 use crate::async_await;
 use crate::initializer;
 use actix_http::Response;
-use actix_web::{web, HttpResponse};
+use actix_web::{error, web, HttpResponse};
 use futures01::stream::Stream;
 
 #[derive(Clone)]
@@ -24,14 +24,14 @@ async fn api_list_users(context: web::Data<WebContext>) -> Result<HttpResponse, 
 async fn api_create_user(
     payload: web::Payload,
     context: web::Data<WebContext>,
-) -> Result<HttpResponse, ()> {
+) -> Result<HttpResponse, error::Error> {
     let body = Box::new(
         futures::compat::Compat01As03::new(payload.concat2())
             .await
-            .map_err(|_| ())?,
+            .map_err(error::ErrorBadRequest)?,
     );
     let input = serde_json::from_slice::<crate::domain::service::UserCreateInput>(body.as_ref())
-        .map_err(|_| ())?;
+        .map_err(error::ErrorBadRequest)?;
 
     context.app.services.user_service.create(input).await;
 

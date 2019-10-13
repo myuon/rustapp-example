50f333964ce443480ab7b8e80bf80af4a18d5973
myuon <ioi.joi.koi.loi@gmail.com>
2019-10-13 22:05:37 +0900

    feat: add heavy sleeping API

--- src/web.rs
+++ src/web.rs
@@ -67,6 +67,10 @@ pub fn handlers(cfg: &mut web::ServiceConfig) {
     .service(
         web::resource("/private/login/{user_id}")
             .route(web::put().to_async(async_await::wrap2(private_api_enable_user_with_password))),
+    )
+    .service(
+        web::resource("/perf/blocking_10")
+            .route(web::get().to_async(async_await::wrap2(api_blocking))),
     );
 }
 
@@ -165,4 +169,17 @@ async fn private_api_enable_user_with_password(
         .map_err(|e| e.to_http_error())?;
 
     Ok(Response::Ok().json(res))
+}
+
+async fn api_blocking(
+    payload: web::Payload,
+    context: web::Data<WebContext>,
+) -> Result<HttpResponse, error::Error> {
+    use diesel::prelude::*;
+    let conn = context.app.infras.conn_pool.get_connection();
+    let _ = diesel::sql_query("SELECT sleep(10)")
+        .execute(&conn)
+        .unwrap();
+
+    Ok(Response::Ok().finish())
 }
